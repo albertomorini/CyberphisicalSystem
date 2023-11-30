a93081496927e2f580e381bcad9b0390bd79491e
albertomorini <99morini@gmail.com>
2023-12-01 00:37:34 +0100

    added android

--- IntrusionDetection/src/components/Dashboard.jsx
+++ IntrusionDetection/src/components/Dashboard.jsx
@@ -1,28 +1,73 @@
-import { IonCol, IonContent, IonItem, IonRow } from '@ionic/react';
+import { IonButton, IonCol, IonContent, IonItem, IonRow, IonText } from '@ionic/react';
 import { useEffect, useState } from 'react';
 import moment from "moment"
 
-const Dashboard = () => {
+import { Storage } from '@ionic/storage';
+import { PushNotifications } from '@capacitor/push-notifications';
+
+
+const Dashboard= () => {
     const [Messages, setMessages] = useState([]);
+    const store = new Storage();
 
-    function getData() {
-        setTimeout(() => {
+    const addListeners = async () => {
+        await PushNotifications.addListener('registration', token => {
+            console.info('Registration token: ', token.value);
+        });
 
-            fetch("http://localhost:1199", {
-                mode: "cors",
-                method: "GET"
-            }).then(res => res.text()).then(res => {
-                console.log(res);
-            })
+        await PushNotifications.addListener('registrationError', err => {
+            console.error('Registration error: ', err.error);
+        });
 
-            let x = Array.from(Array(15).keys());
-            console.log(x);
-            setMessages([...Messages, ...x])
+        await PushNotifications.addListener('pushNotificationReceived', notification => {
+            console.log('Push notification received: ', notification);
+        });
+
+        await PushNotifications.addListener('pushNotificationActionPerformed', notification => {
+            console.log('Push notification action performed', notification.actionId, notification.inputValue);
+        });
+    }
+
+    const registerNotifications = async () => {
+        let permStatus = await PushNotifications.checkPermissions();
+
+        if (permStatus.receive === 'prompt') {
+            permStatus = await PushNotifications.requestPermissions();
+        }
+
+        if (permStatus.receive !== 'granted') {
+            throw new Error('User denied permissions!');
+        }
+
+        await PushNotifications.register();
+    }
+
+    const getDeliveredNotifications = async () => {
+        const notificationList = await PushNotifications.getDeliveredNotifications();
+        console.log('delivered notifications', notificationList);
+    }
+
+    async function getData() {
+        //await store.create();
+
+        setInterval(() => {
+
+            // fetch("http://localhost:1199", {
+            //     mode: "cors",
+            //     method: "GET"
+            // }).then(res => res.json()).then(res => {
+            //     console.log(res.data);
+            //     let x = Messages.concat(res.data)
+            //     setMessages(x)
+            // })
 
         }, 2000);
     }
 
     useEffect(() => {
+        addListeners();
+        registerNotifications();
+        getDeliveredNotifications();
         getData();
     }, [])
 
@@ -40,16 +85,21 @@ const Dashboard = () => {
                     <IonRow>
                         <IonItem style={{ width: "100%" }}>
                             <IonCol>
-                                {moment().format("DD/MM/YYYY HH:mm:ss")}
+                                {moment(s.timestamp).format("DD/MM/YYYY HH:mm:ss")}
                             </IonCol>
                             <IonCol>
-                                {s}
+                                {s.message}
                             </IonCol>
                         </IonItem>
                     </IonRow>
                 ))}
             </IonContent>
 
+            <IonButton color="danger" expand='block'>Clear cache</IonButton>
+            <h3 className='ion-text-center'>
+                Made by Alberto Morini & Davide Bassan
+            </h3>
+            
         </IonContent>
     )
 }
--- server/server.py
+++ server/server.py
@@ -1,17 +1,31 @@
+import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 PORT = 1199
+
+# a Python object (dict):
+message = {
+    "data": []
+}
+
+message["data"].append({
+  "timestamp": "2023/11/30 12:32:33",
+  "message": "malicious detection",
+})
+
+
 
 ## class will handle the requests
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self): ##GET REQUESTS
         self.send_response(200)
         # TODO: set JSON
-        self.send_header('Content-type','text/html')
+        self.send_header('Content-type','application/json')
         self.send_header('Access-Control-Allow-Origin','*')
         self.end_headers()
         # TODO: retrive data from backend 
-        message = "Attack detected!"
-        self.wfile.write(bytes(message, "utf8"))
+
+        y = json.dumps(message)
+        self.wfile.write(bytes(y, "utf8"))
 
 
 def run():
