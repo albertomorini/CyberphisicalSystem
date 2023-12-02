658b74fe350a6acdf6b48614f47d436d33b774a4
albertomorini <99morini@gmail.com>
2023-12-02 17:15:59 +0100

    added backend

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import utils
 
+limitNrDetections = 10
+
 ids = utils.extract_id('dataset/Attack_free_dataset.txt')
 matrix, aux_ids = utils.generate_matrix(ids)
+print(aux_ids) ## unique IDS
 
-print(aux_ids)
-data = utils.extract_everything('dataset/Impersonation_attack_dataset.txt')
-saved_msg = utils.analyze_traffic(matrix, aux_ids, data)
+datasetExtractions = {
+    "DoS" : utils.extract_everything('dataset/DoS_attack_dataset.txt'),
+    "Fuzzy" : utils.extract_everything('dataset/Fuzzy_attack_dataset.txt'),
+    "Impersonate" : utils.extract_everything('dataset/Impersonation_attack_dataset.txt'),
+}
+detections = {
+    "DoS" : utils.analyze_traffic(matrix,aux_ids, datasetExtractions["DoS"]),
+    "Fuzzy" : utils.analyze_traffic(matrix,aux_ids, datasetExtractions["Fuzzy"]),
+    "Impersonate" : utils.analyze_traffic(matrix,aux_ids, datasetExtractions["Impersonate"]),
+}
 
-print(len(saved_msg))
+
+dataToSend = []
+
+for i in detections:    ## for each attack
+    for j in range(limitNrDetections): ## [JUST FOR TEST] limit to a fixed number of detection in way to send few message to the client
+        detections[i][j]["dataset"]= i
+        dataToSend.append(detections[i][j])
+
+
+print(dataToSend)
